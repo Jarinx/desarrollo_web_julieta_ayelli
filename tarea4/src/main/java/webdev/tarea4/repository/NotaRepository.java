7b2686ef805f9bce76b9e2016172c7e4e4b3e8b0
Jarinx <144623039+Jarinx@users.noreply.github.com>
2025-11-17 14:05:51 -0300

    repositories listos

---
+++ tarea4/src/main/java/webdev/tarea4/repository/NotaRepository.java
@@ -0,0 +1,10 @@
+package webdev.tarea4.repository;
+
+import webdev.tarea4.model.Nota;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotaRepository extends JpaRepository<Nota, Integer> {
+    
+}
