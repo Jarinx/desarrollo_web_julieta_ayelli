d6aa3aa594f8bc314e5e870a0875703eb888c9fd
Jarinx <144623039+Jarinx@users.noreply.github.com>
2025-11-17 13:51:16 -0300

    resources: sql files

---
+++ tarea4/src/main/resources/schema.sql
@@ -0,0 +1,149 @@
+-- -----------------------------------------------------
+-- TABLAS PARA LA DB 'tarea4'
+-- -----------------------------------------------------
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Drop Schema tarea4
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `tarea4` ;
+
+-- -----------------------------------------------------
+-- Create Schema tarea4
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `tarea4` DEFAULT CHARACTER SET utf8 ;
+USE `tarea4` ;
+
+-- -----------------------------------------------------
+-- Table `tarea4`.`region`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tarea4`.`region` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(200) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `tarea4`.`comuna`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tarea4`.`comuna` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(200) NOT NULL,
+  `region_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_comuna_region1_idx` (`region_id` ASC),
+  CONSTRAINT `fk_comuna_region1`
+    FOREIGN KEY (`region_id`)
+    REFERENCES `tarea4`.`region` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `tarea4`.`aviso_adopcion`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tarea4`.`aviso_adopcion` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `fecha_ingreso` DATETIME NOT NULL,
+  `comuna_id` INT NOT NULL,
+  `sector` VARCHAR(100) NULL,
+  `nombre` VARCHAR(200) NOT NULL,
+  `email` VARCHAR(100) NOT NULL,
+  `celular` VARCHAR(15) NULL,
+  `tipo` ENUM('gato', 'perro') NOT NULL,
+  `cantidad` INT NOT NULL,
+  `edad` INT NOT NULL,
+  `unidad_medida` ENUM('a', 'm') NOT NULL,
+  `fecha_entrega` DATETIME NOT NULL,
+  `descripcion` VARCHAR(500) NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_aviso_comuna1_idx` (`comuna_id` ASC),
+  CONSTRAINT `fk_aviso_comuna1`
+    FOREIGN KEY (`comuna_id`)
+    REFERENCES `tarea4`.`comuna` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `tarea4`.`foto`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tarea4`.`foto` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `ruta_archivo` VARCHAR(300) NOT NULL,
+  `nombre_archivo` VARCHAR(300) NOT NULL,
+  `aviso_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `aviso_id`),
+  INDEX `fk_foto_aviso1_idx` (`aviso_id` ASC),
+  CONSTRAINT `fk_foto_aviso1`
+    FOREIGN KEY (`aviso_id`)
+    REFERENCES `tarea4`.`aviso_adopcion` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `tarea4`.`contactar_por`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tarea4`.`contactar_por` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nombre` ENUM('whatsapp', 'telegram', 'X', 'instagram', 'tiktok', 'otra') NOT NULL,
+  `identificador` VARCHAR(150) NOT NULL,
+  `aviso_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `aviso_id`),
+  INDEX `fk_contactar_por_aviso1_idx` (`aviso_id` ASC),
+  CONSTRAINT `fk_contactar_por_aviso1`
+    FOREIGN KEY (`aviso_id`)
+    REFERENCES `tarea4`.`aviso_adopcion` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `tarea4`.`comentario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tarea4`.`comentario` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(80) NOT NULL,
+  `texto` VARCHAR(300) NOT NULL,
+  `fecha` TIMESTAMP NOT NULL,
+  `aviso_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_comentario_aviso1_idx` (`aviso_id` ASC),
+  CONSTRAINT `fk_comentario_aviso1`
+    FOREIGN KEY (`aviso_id`)
+    REFERENCES `tarea4`.`aviso_adopcion` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `tarea4`.`nota`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tarea4`.`nota` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `aviso_id` INT NOT NULL,
+  `nota` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_nota_aviso1_idx` (`aviso_id` ASC),
+  CONSTRAINT `fk_nota_aviso1`
+    FOREIGN KEY (`aviso_id`)
+    REFERENCES `tarea4`.`aviso_adopcion` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
